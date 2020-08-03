1b3bc6da0551a273b2cf3419d544fbd51127b3c2
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-03 18:28:23 +0100

    Created third test

--- spec/capitalize_spec.rb
+++ spec/capitalize_spec.rb
@@ -6,4 +6,7 @@ describe 'capitalize' do
 	it 'returns Word when passed word' do
 		expect(capitalize('word')).to eq "Word"
 	end
+	it 'returns Word when passed wOrD' do
+		expect(capitalize('wOrD')).to eq "Word"
+	end
 end
